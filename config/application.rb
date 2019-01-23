298445c5d37a3a421f666248531db0de7d328803
Keren-Happuc Fleurimond <keren@noukod.com>
2019-01-23 15:24:15 +0000

    Done.

--- config/application.rb
+++ config/application.rb
@@ -13,8 +13,8 @@ require "sprockets/railtie"
 
 # Require the gems listed in Gemfile, including any gems
 # you've limited to :test, :development, or :production.
-Bundler.require(*Rails.groups)
 
+Bundler.require(*Rails.groups)
 module BlogFlash
   class Application < Rails::Application
     # Settings in config/environments/* take precedence over those specified here.
@@ -28,9 +28,7 @@ module BlogFlash
     # The default locale is :en and all translations from config/locales/*.rb,yml are auto loaded.
     # config.i18n.load_path += Dir[Rails.root.join('my', 'locales', '*.{rb,yml}').to_s]
     # config.i18n.default_locale = :de
-
     # Do not swallow errors in after_commit/after_rollback callbacks.
     config.active_record.raise_in_transactional_callbacks = true
-    config.action_controller.permit_all_parameters = true
   end
 end
